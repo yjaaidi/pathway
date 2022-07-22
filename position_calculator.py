f5dc1be31fedb300ba8c1768dac5859008ec8ced
Younes Jaaidi <yjaaidi@gmail.com>
2022-07-22 16:05:14 +0200

    refactor: 🛠 compute position

--- position_calculator.py
+++ position_calculator.py
@@ -14,9 +14,13 @@ class PositionCalculator:
     height: int
 
     def compute_position(self, box_points: tuple[int, int, int, int]) -> Position:
-        """@deprecated: 🚧work in progress"""
         x1, y1, x2, y2 = box_points
-        x = (x1 + x2) / 2
+
+        # 1. Compute center (x1 + x2) / 2
+        # 2. Translate to the left (-self.width / 2)
+        # 3. Make it a percentage
+        x = round(((x1 + x2 - self.width) / 2) * 100 / (self.width / 2))
+
         height = round((y2 - y1)/self.height * 100)
         width = round((x2 - x1)/self.width * 100)
         return Position(x=x, height=height, width=width)
