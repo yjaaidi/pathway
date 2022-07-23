0bf9a0ab49a009f15688430b43ee3f4ac0cae311
Younes Jaaidi <yjaaidi@gmail.com>
2022-07-23 09:09:01 +0200

    build: 📦 download more models

--- download-models.sh
+++ download-models.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env sh
 
-wget https://raw.githubusercontent.com/khinthetnwektn/ObjectsDetectionTesting1/eeaf85c9658f13de9a0d32e9e54418395023213f/models/yolo-tiny.h5 -P models
-wget https://github.com/OlafenwaMoses/ImageAI/releases/download/1.0/yolo.h5 -P models
+for f in https://github.com/OlafenwaMoses/ImageAI/releases/download/essentials-v5/resnet50_coco_best_v2.1.0.h5 \
+https://raw.githubusercontent.com/khinthetnwektn/ObjectsDetectionTesting1/eeaf85c9658f13de9a0d32e9e54418395023213f/models/yolo-tiny.h5 \
+https://github.com/OlafenwaMoses/ImageAI/releases/download/1.0/yolo.h5;
+do
+    rm -f $f
+    wget $f -P models
+done
