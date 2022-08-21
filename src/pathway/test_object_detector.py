f8fb75407d524257ef37b1eb6532325c1c5bf083
Younes Jaaidi <yjaaidi@gmail.com>
2022-08-21 18:12:36 +0200

    test: ✅ add empty ObjectDetector test

---
+++ src/pathway/test_object_detector.py
@@ -0,0 +1,6 @@
+
+def test_detect_objects():
+  # @todo load JPEG picture with Pillow
+  # @todo ObjectDetector().detect_objects(picture=picture)
+  # @todo check 4 beatles detected with right positions
+  pass
