f831f64762fbae0ab1aac37192a6ee1295f9850e
Younes Jaaidi <yjaaidi@gmail.com>
2022-08-21 20:31:25 +0200

    refactor: 🛠 implement ObjectDetector

--- src/pathway/object_detector.py
+++ src/pathway/object_detector.py
@@ -2,7 +2,7 @@ import io
 from dataclasses import dataclass
 from os import getcwd
 from os.path import join
-from typing import List, Literal
+from typing import List, Literal, Optional
 
 import numpy
 from PIL import Image
@@ -14,6 +14,7 @@ from pathway.position_calculator import Position, PositionCalculator
 @dataclass
 class DetectedObject:
   type: str
+  probability: float
   position: Position
 
 class ObjectDetector:
@@ -21,7 +22,7 @@ class ObjectDetector:
   _height = 480
 
 
-  def detect_objects(self, picture_data: bytes) -> List[DetectedObject]:
+  def detect_objects(self, picture_data: bytes, filtered_types: List[str] = None) -> List[DetectedObject]:
     """
     Detect objects in the given picture.
 
@@ -42,14 +43,20 @@ class ObjectDetector:
       input_image=picture_array,
       input_type="array",
       output_type="array",
-      minimum_percentage_probability=40,
+      minimum_percentage_probability=10,
     )[1]
 
-    return list(map(lambda item: self._to_object(detected_item=item), detected_items))
+    detected_objects = [self._to_object(detected_item=item) for item in detected_items]
+
+    if filtered_types is not None:
+      detected_objects = [detected_object for detected_object in detected_objects if detected_object.type in filtered_types]
+    
+    return detected_objects
 
   def _to_object(self, detected_item: DetectedItem) -> DetectedObject:
     position_calculator = PositionCalculator(width=self._width, height=self._height)
     return DetectedObject(
       type=detected_item['name'],
-      position=position_calculator.compute_position(box_points=detected_item['box_points'])
+      position=position_calculator.compute_position(box_points=detected_item['box_points']),
+      probability=detected_item['percentage_probability'],
     )
--- src/pathway/test_object_detector.py
+++ src/pathway/test_object_detector.py
@@ -1,11 +1,13 @@
 
-import pytest
 from pathway.object_detector import ObjectDetector
+from pathway.position_calculator import Position
 
-@pytest.mark.skip(reason="🚧 work in progress")
 def test_detect_objects():
   with open("src/pathway/beatles.jpg", "rb") as f:
     picture_data = f.read()
-    detected_objects = ObjectDetector().detect_objects(picture_data=picture_data)
+    detected_objects = ObjectDetector().detect_objects(picture_data=picture_data, filtered_types=["person"])
     assert len(detected_objects) == 4
-    # @todo check 4 beatles detected with right positions
+    assert detected_objects[0].position == Position(x=-77, width=13, height=42)
+    assert detected_objects[1].position == Position(x=-37, width=13, height=42)
+    assert detected_objects[2].position == Position(x=-10, width=15, height=41)
+    assert detected_objects[3].position == Position(x=37, width=14, height=39)
