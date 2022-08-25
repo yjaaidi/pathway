64e31f6bd3fab61e1b96c58ea9dd73da2d9aaf28
Younes Jaaidi <yjaaidi@gmail.com>
2022-08-26 00:13:23 +0200

    refactor: 🛠 fix typing

--- src/pathway_client/led_strategy.py
+++ src/pathway_client/led_strategy.py
@@ -1,4 +1,9 @@
 
+from typing import List
+from rx.core.typing import Observable
+from pathway_shared.detected_object import DetectedObject
+
+
 class LedStrategy:
 
     _led_count: int
@@ -6,5 +11,5 @@ class LedStrategy:
     def __init__(self, led_count: int):
         self._led_count = led_count
 
-    def get_leds_obs(detected_objects):
+    def get_leds_obs(self, detected_objects_obs: Observable[List[DetectedObject]]):
         raise NotImplementedError()
