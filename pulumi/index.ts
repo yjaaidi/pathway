5319de319568467138285b71dbd86f2991bad421
Younes Jaaidi <yjaaidi@gmail.com>
2022-08-26 15:02:13 +0200

    build: 🛠 deploy hello image to cloud run

--- pulumi/index.ts
+++ pulumi/index.ts
@@ -1,10 +1,47 @@
-// import * as pulumi from "@pulumi/pulumi";
-// import * as gcp from "@pulumi/gcp";
+import * as pulumi from '@pulumi/pulumi';
+import * as gcp from '@pulumi/gcp';
 
-// // Create a GCP resource (Storage Bucket)
-// const bucket = new gcp.storage.Bucket("my-bucket", {
-//     location: "US"
-// });
+const activateCloudRun = new gcp.projects.Service('cloud-run', {
+  disableDependentServices: true,
+  service: 'run.googleapis.com',
+});
 
-// // Export the DNS name of the bucket
-// export const bucketName = bucket.url;
+const pathwayService = new gcp.cloudrun.Service(
+  'pathway-service',
+  {
+    location: 'europe-west1',
+    template: {
+      spec: {
+        containers: [
+          {
+            image: 'gcr.io/cloudrun/hello',
+          },
+        ],
+      },
+    },
+    traffics: [
+      {
+        percent: 100,
+        latestRevision: true,
+      },
+    ],
+  },
+  {
+    dependsOn: [activateCloudRun],
+  }
+);
+
+const publicCloudRunPolicy = gcp.organizations.getIAMPolicy({
+  bindings: [
+    {
+      role: 'roles/run.invoker',
+      members: ['allUsers'],
+    },
+  ],
+});
+
+new gcp.cloudrun.IamPolicy('publicCloudRunPolicy', {
+  location: pathwayService.location,
+  service: pathwayService.name,
+  policyData: publicCloudRunPolicy.then((policy) => policy.policyData),
+});
